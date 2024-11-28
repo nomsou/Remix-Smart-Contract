47346ef49e1564efb29b9145b50bd1917d48f678
nomsou <nomsounaogu@gmail.com>
2024-11-28 11:26:44 +0100

    initial commit

---
+++ contracts/Crowdfunding.sol
@@ -0,0 +1,54 @@
+ //SPDX-License-Identifier: MIT
+pragma solidity^0.8.0;
+
+ contract Crowdfunding {
+    string public name;
+    string public description;
+    uint256 public goal;
+    uint256 public deadline;
+    address public owner;
+
+    struct Tier {
+        string name;
+        uint256 amount;
+        uint256 backers;
+    }
+
+    Tier[] public tiers;
+
+//Runs once the smart contract is deployed
+    constructor (
+    string memory _name,
+    string memory _description,
+    uint256 _goal,
+    uint256 _durationInDays
+    ) {
+        name = _name;
+        description = _description;
+        goal = _goal;
+        deadline = block.timestamp + (_durationInDays * 1 days);
+        owner = msg.sender;
+    }
+
+    function fund()public payable{
+        require(msg.value > 0, "Must fund amount greater than 0.");
+        require(block.timestamp < deadline, "Campaign has ended.");
+
+
+    }
+
+    function withdraw() public{
+        require(msg.sender == owner, "Only the owner can withdraw.");
+        require(address(this).balance >= goal,"Goal has not been reached");
+
+        uint256 balance = address(this).balance;
+        require(balance > 0, "No balance to withdraw");
+
+        payable(owner).transfer(balance);
+    }
+
+    function getContractBalance() public view returns(uint256)  {
+        return address(this).balance;
+
+    }
+ }
