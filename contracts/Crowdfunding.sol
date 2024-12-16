3c41aa15f7f670e8a3227a28c7fe2222f9cd0667
nomsou <nomsounaogu@gmail.com>
2024-12-16 22:10:09 +0100

    factory contract code

--- contracts/Crowdfunding.sol
+++ contracts/Crowdfunding.sol
@@ -7,6 +7,10 @@ pragma solidity^0.8.0;
     uint256 public goal;
     uint256 public deadline;
     address public owner;
+    bool public paused;
+
+    enum CampaignState{Active, Successful, Failed}
+    CampaignState public state;
 
     struct Tier {
         string name;
@@ -14,15 +18,34 @@ pragma solidity^0.8.0;
         uint256 backers;
     }
 
+//Keep information of how much a wallet address has actually funded or backed a campaign
+    struct Backer{
+        uint256 totalContribution;
+        mapping(uint256 => bool) fundedTiers;
+
+    }
+
     Tier[] public tiers;
+    mapping(address => Backer) public backers;
 
     modifier onlyOwner(){
         require(msg.sender == owner, "Not the owner");
         _;  
     }
 
+    modifier campaignOpen() {
+        require(state == CampaignState.Active, "Campaign is not active");
+        _;
+    }
+
+    modifier notPaused(){
+        require(!paused, "Contract is paused.");
+        _;
+    }
+
 //Runs once the smart contract is deployed
     constructor (
+    address _owner,
     string memory _name,
     string memory _description,
     uint256 _goal,
@@ -32,7 +55,18 @@ pragma solidity^0.8.0;
         description = _description;
         goal = _goal;
         deadline = block.timestamp + (_durationInDays * 1 days);
-        owner = msg.sender;
+        owner = _owner;
+        state = CampaignState.Active;
+    }
+
+    function checkAndUpdateCampaignState() internal {
+        if(state == CampaignState.Active){
+            if(block.timestamp >= deadline){
+                state = address(this).balance >= goal ? CampaignState.Successful : CampaignState.Failed;
+            } else {
+                state = address(this).balance >= goal ? CampaignState.Successful : CampaignState.Active;
+            }
+        }
     }
 
 
@@ -45,15 +79,17 @@ pragma solidity^0.8.0;
     // }
 
     //Code to fund tiers
-    function fund(uint256 _tierIndex)public payable{
-        require(block.timestamp < deadline, "Campaign has ended.");
+    function fund(uint256 _tierIndex)public payable campaignOpen notPaused{
         require(_tierIndex < tiers.length, "Invalid tier");
         require(msg.value == tiers[_tierIndex].amount, "Incorrect amount");
 
 
         //Code to increase value of backers
         tiers[_tierIndex].backers++;
+        backers[msg.sender].totalContribution += msg.value;
+        backers[msg.sender].fundedTiers[_tierIndex] = true;
 
+        checkAndUpdateCampaignState();
 
     }
 
@@ -72,7 +108,8 @@ pragma solidity^0.8.0;
     }
 
     function withdraw() public onlyOwner{
-        require(address(this).balance >= goal,"Goal has not been reached");
+        checkAndUpdateCampaignState();
+        require(state == CampaignState.Successful, "Campaign not Successful");
 
         uint256 balance = address(this).balance;
         require(balance > 0, "No balance to withdraw");
@@ -83,5 +120,38 @@ pragma solidity^0.8.0;
     function getContractBalance() public view returns(uint256)  {
         return address(this).balance;
 
+    }
+
+    function refund() public {
+        checkAndUpdateCampaignState();
+        require(state == CampaignState.Failed, "Refunds not available");
+        uint256 amount = backers[msg.sender].totalContribution;
+        require(amount > 0, "No contribution to refund");
+
+        backers[msg.sender].totalContribution = 0;
+        payable(msg.sender).transfer(amount);
+    }
+
+    function hasFundedTier(address _backer, uint256 _tierIndex) public view returns (bool){
+        return backers[_backer].fundedTiers[_tierIndex];
+    }
+
+    function getTiers() public view returns (Tier[] memory){
+        return tiers;
+    }
+
+    function togglePause() public onlyOwner{
+        paused = !paused;
+    }
+
+    function getCampaignStatus() public view returns (CampaignState) {
+        if (state == CampaignState.Active && block.timestamp > deadline){
+            return address(this).balance >= goal ? CampaignState.Successful : CampaignState.Failed;
+        }
+        return state;
+    }
+
+    function extendDeadline(uint256 _daysToAdd) public onlyOwner campaignOpen {
+        deadline += _daysToAdd * 1 days;
     }
  }
